5e65bef0daa3276fb01fd469f3854e774d725d52
Dave Voutila <voutilad@gmail.com>
2017-01-14 22:17:13 -0500

    added intial lambda-only solution using Meetup API key (in .env) and some tests using mock

--- btvpython/alexa.py
+++ btvpython/alexa.py
@@ -1,6 +1,33 @@
 """
 Our Alexa Skill for BTVPython
 """
+import os
+import json
+import urllib2
+
+MEETUP_ENDPOINT = 'https://api.meetup.com/{group}/events?sign=false&key={key}&photo-host=public&page=1'
+
+def get_next_meetup(group_name):
+    """
+    Lookup the next Meetup event for a group.
+    :return: dict with details about the next event
+    """
+    meetup = {}
+
+    url = MEETUP_ENDPOINT.format(group=group_name, key=os.environ['MEETUP_API_KEY'])
+    response = json.loads(urllib2.urlopen(url).read())
+
+    if response:
+        meetup['title'] = response[0]['name']
+        meetup['date'] = response[0]['time']
+        meetup['venue'] = response[0]['venue']['name']
+    return meetup
+
 
 def lambda_handler(event, context):
-    return "Hello World!"
+    msg = 'The next Burlington Python meetup is "{title}" on {date} at {venue}'
+    meetup = get_next_meetup('btvpython')
+    if meetup:
+        return msg.format(**meetup)
+    else:
+        return 'Sorry, I could not find an upcoming event for Burlington Python.'
--- tests/test_alexa.py
+++ tests/test_alexa.py
@@ -1,8 +1,30 @@
 """
 Tests for our Alexa Skill
 """
+import os
+from mock import patch
+from dotenv import load_dotenv
 from btvpython import alexa
 
 
+def setup_module(module):
+    load_dotenv('.env')
+
+
 def test_lambda_handler():
-    assert alexa.lambda_handler(None, None) == 'Hello World!'
+    response = alexa.lambda_handler(None, None)
+    assert 'The next Burlington Python meetup is' in response
+
+
+@patch('btvpython.alexa.get_next_meetup', return_value={})
+def test_no_meeting_response(mock):
+    response = alexa.lambda_handler(None, None)
+    assert 'Sorry, I could not find an upcoming event' in response
+
+
+def test_meetup_client():
+    assert 'MEETUP_API_KEY' in os.environ
+    next_meetup = alexa.get_next_meetup('btvpython')
+    assert 'title' in next_meetup
+    assert 'date' in next_meetup
+    assert 'venue' in next_meetup
